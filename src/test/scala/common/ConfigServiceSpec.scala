1b383c66b36b9886a0e0211ec4890270c897a28b
me-mee3 <mrkc1008@gmail.com>
2020-12-02 23:41:04 +0900

    [対応]#2-テストの作成

---
+++ src/test/scala/common/ConfigServiceSpec.scala
@@ -0,0 +1,11 @@
+import collection.mutable.Stack
+import org.scalatest._
+import flatspec._
+import matchers._
+
+class ConfigServiceSpec extends AnyFlatSpec with should.Matchers {
+
+  "Config Service" should "resource.txt記載の1行が「A=B」の場合、Bを取得する" in {
+
+  }
+}
